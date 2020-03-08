f905c8caf2cb2d79c23c211e1b27a2c9cb8c7391
Dev Govindji <friendsofdev94@gmail.com>
2020-03-08 16:44:51 -0500

    form submits with react hooks

--- src/Components/NameForm.jsx
+++ src/Components/NameForm.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react"
 
 export default function NameForm(props) {
+    const [name, setName] = useState("")
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        alert('hello')
+        alert(`Submitting ${name}`)
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <label>
-                Name: <input type="text" />
+                Name: <input type="text" value={name} onChange={e => setName(e.target.value)} />
             </label>
 
             <input type="submit" value="Submit" />

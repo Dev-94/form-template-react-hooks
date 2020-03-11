54f843bfe0b5ce35ada6112287eed48c1285bf15
Dev Govindji <friendsofdev94@gmail.com>
2020-03-11 16:02:41 -0500

    adds name validation

--- src/Components/NameForm.jsx
+++ src/Components/NameForm.jsx
@@ -9,14 +9,28 @@ export default function NameForm(props) {
     const { value: lastName, bind: bindLastName, reset: resetLastName } = useInput('')
 
     const handleSubmit = (event) => {
-        event.preventDefault()
         alert(`Submitting ${firstName} ${lastName}`)
         resetFirstName()
         resetLastName()
+
+    }
+    /* PSEUDOCODE FOR IMPLEMENTING NAME VALIDATION */
+    // before it is submitted, it must be validated; after it is typed, it must be validated
+    // event.preventDefault() has to happen before everything else
+    // bindFirstName is the input
+    // input gets passed into validation function 
+    // return true, handle submit fires off
+    // return false, sends alert message 'please input valid name'
+
+    const validateName = (event, inputFirstName = bindFirstName.value, inputLastName = bindLastName.value) => {
+        event.preventDefault()
+        const regex = /^\D{2,}$/
+        if (regex.test(inputFirstName) || regex.test(inputLastName)) handleSubmit()
+        else alert('Please input a valid name.')
     }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={validateName}>
             <label>
                 First Name: <input type="text" {...bindFirstName} />
             </label>
